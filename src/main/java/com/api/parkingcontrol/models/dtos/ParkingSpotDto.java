db187a9da9f812095932afda806a696ffee94800
aevilesaguiar <aeviles@hotmail.com>
2022-04-19 15:19:29 -0300

    atualizando incluindo método POST

---
+++ src/main/java/com/api/parkingcontrol/models/dtos/ParkingSpotDto.java
@@ -0,0 +1,35 @@
+package com.api.parkingcontrol.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParkingSpotDto {
+
+    //anottations do spring validation
+    @NotBlank
+    private String parkingSpotNumber;//numero da vaga não deve ser vazio
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+    @NotBlank
+    private String brandCar;
+    @NotBlank
+    private String modelCar;
+    @NotBlank
+    private String colorCar;
+    @NotBlank
+    private String responsibleName;
+    @NotBlank
+    private String apartment;
+    @NotBlank
+    private String block;
+
+
+}
